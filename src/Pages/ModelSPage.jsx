aa2a76cf96a63af71cd12de9c434f2bc4f5e7df4
Eugen Oswald <eugen.oswald@kemweb.de>
2023-09-21 15:38:55 +0200

    add pages+components

---
+++ src/Pages/ModelSPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ModelSPage = () => {
+  return <div>ModelS</div>;
+};
+
+export default ModelSPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './Pages/HomePage.jsx';
+import ModelSPage from './Pages/ModelSPage.jsx';
+import ModelXPage from './Pages/ModelXPage.jsx';
+import CybertruckPage from './Pages/CybertruckPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/model_s',
+    element: <ModelSPage />,
+  },
+  {
+    path: '/model_x',
+    element: <ModelXPage />,
+  },
+  {
+    path: '/cybertruck',
+    element: <CybertruckPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
